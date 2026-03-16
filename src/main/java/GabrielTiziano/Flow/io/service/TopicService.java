57b2933056ead124785ad45942dfcb07dc653a62
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-16 09:18:42 -0300

    Injeta a dependência do Mapper no Service e implementa os DTOs nos métodos ao invés de trabalhar direto com a Model

--- src/main/java/GabrielTiziano/Flow/io/service/TopicService.java
+++ src/main/java/GabrielTiziano/Flow/io/service/TopicService.java
@@ -1,38 +1,48 @@
 package GabrielTiziano.Flow.io.service;
 
+import GabrielTiziano.Flow.io.dto.TopicDTO;
+import GabrielTiziano.Flow.io.mapper.TopicMapper;
 import GabrielTiziano.Flow.io.model.Topic;
 import GabrielTiziano.Flow.io.repository.TopicRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class TopicService {
     private final TopicRepository topicRepository;
+    private final TopicMapper topicMapper;
 
-    public TopicService(TopicRepository topicRepository) {
+    public TopicService(TopicRepository topicRepository, TopicMapper topicMapper) {
         this.topicRepository = topicRepository;
+        this.topicMapper = topicMapper;
     }
 
-    public List<Topic> getTopics(){
-        return topicRepository.findAll();
+    public List<TopicDTO> getTopics(){
+        List<Topic> topicsFounded = topicRepository.findAll();
+        return topicsFounded.stream().map(topicMapper::map).collect(Collectors.toList());
     }
 
-    public Topic createTopic(Topic topic){
-        return topicRepository.save(topic);
+    public TopicDTO createTopic(TopicDTO topicDTO){
+        Topic topicModel = topicMapper.map(topicDTO);
+        topicModel = topicRepository.save(topicModel);
+        return topicMapper.map(topicModel);
     }
 
-    public Topic getTopic(Long id){
+    public TopicDTO getTopic(Long id){
         Optional<Topic> topicFound = topicRepository.findById(id);
-        return topicFound.orElse(null);
+        return topicFound.map(topicMapper::map).orElse(null);
     }
 
-    public Topic updateTopic(Long id, Topic topicSent){
+    public TopicDTO updateTopic(Long id, TopicDTO topicSent){
         Optional<Topic> topicFound = topicRepository.findById(id);
         if(topicFound.isPresent()){
-            topicSent.setId(id);
-            topicRepository.save(topicSent);
+            Topic topicModel = topicMapper.map(topicSent);
+            topicModel.setId(id);
+            Topic topicUpdated = topicRepository.save(topicModel);
+            return topicMapper.map(topicUpdated);
         } else {
             return null;
         }
